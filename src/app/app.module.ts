3e379b5a16fba1a1aa5a05d4948304c3145ec845
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-11-21 16:44:28 -0300

    feat: create manual and automatic module using the nest cli

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConceptsManualModule } from 'src/concepts-manual/concepts-manual.module';
+import { ConceptsAutomaticModule } from 'src/concepts-automatic/concepts-automatic.module';
+
+@Module({
+  imports: [ConceptsManualModule, ConceptsAutomaticModule],
+  controllers: [AppController],
+  providers: [AppService],
+  exports: [], 
+})
+export class AppModule {}
