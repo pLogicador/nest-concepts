272cff89b4a1f4cea8a21ccddc9ed4ab00c93f65
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-11-23 00:15:53 -0300

    feat: configure ValidationPipe with useGlobalPipes for create message validation

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateMessageDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
   readonly text: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly from: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(50)
   readonly to: string;
 }
