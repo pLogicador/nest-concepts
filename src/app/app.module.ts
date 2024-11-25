804e5e112646d5591f199f801c60b2ea210c61f2
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-11-25 10:48:51 -0300

    chore: configure PostgreSQL and TypeORM integration with DBeaver on Windows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
