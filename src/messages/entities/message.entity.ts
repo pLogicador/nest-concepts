a3e91e147eaabd75c2452c4f6c1013a4e0d45013
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-11-25 11:48:45 -0300

    feat: add initial entity with TypeORM integration for PostgreSQL

--- src/messages/entities/message.entity.ts
+++ src/messages/entities/message.entity.ts
@@ -1,8 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class MessageEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   text: string;
+
+  @Column({ type: 'varchar', length: 50 })
   from: string;
+
+  @Column({ type: 'varchar', length: 50 })
   to: string;
+
+  @Column({ default: false })
   read: boolean;
-  date: Date;
+
+  @Column()
+  date: Date; // createdAt
+
+  @CreateDateColumn()
+  createdAt?: Date; // createdAt
+
+  @UpdateDateColumn()
+  updatedAt?: Date; // updatedAt
 }
