87adbb95858051cd5bb35b261340487276db8bef
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-11-26 19:45:06 -0300

    feat: use onDelete and onUpdate CASCADE to propagate changes in relationships

--- src/messages/entities/message.entity.ts
+++ src/messages/entities/message.entity.ts
@@ -30,13 +30,13 @@ export class MessageEntity {
   updatedAt?: Date; // updatedAt
 
   //Many messages can be sent by one person (sender)
-  @ManyToOne(() => Person)
+  @ManyToOne(() => Person, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
   // Specifies the "from" column that stores the ID of the person who sent the message
   @JoinColumn({ name: 'from' })
   from: Person;
 
   //Many messages can be sent to one person (recipient)
-  @ManyToOne(() => Person)
+  @ManyToOne(() => Person, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
   // Specifies the "to" column that stores the ID of the person receiving the message
   @JoinColumn({ name: 'to' })
   to: Person;
