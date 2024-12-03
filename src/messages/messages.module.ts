8bcea80210c8485ccaa6b241af1e42be4ea6cc4e
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-12-02 21:33:48 -0300

    refactor: cleaned MessagesModule and fixed syntax with lint

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -5,16 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessageEntity } from './entities/message.entity';
 import { PersonsModule } from 'src/persons/persons.module';
 import { MessagesUtils } from './messages.utils';
-import { MyDynamicModule } from 'src/my-dynamic/my-dynamic.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([MessageEntity]),
     forwardRef(() => PersonsModule),
-    MyDynamicModule.register({
-      apiKey: 'Here comes the Key API',
-      apiUrl: 'http://apiurl.any',
-    }),
   ],
   controllers: [MessagesController],
   providers: [MessagesService, MessagesUtils],
