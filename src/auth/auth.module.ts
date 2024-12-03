11e7e0690196b0264883ee738c4ffcd756ab6e31
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-12-03 18:27:25 -0300

    feat(auth): implement user authentication with email, password, and HashingService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from 'src/persons/entities/person.entity';
 
 @Global()
 @Module({
+  imports: [TypeOrmModule.forFeature([Person])],
   controllers: [AuthController],
   providers: [
     {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { Repository } from 'typeorm';
+import { Person } from 'src/persons/entities/person.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from './hashing/hashing.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(Person)
+    private readonly personRepository: Repository<Person>,
+    private readonly hashingService: HashingService,
+  ) {}
+
   async login(LoginDto: LoginDto) {
-    return LoginDto;
+    const person = await this.personRepository.findOneBy({
+      email: LoginDto.email,
+    });
+
+    if (!person) {
+      throw new UnauthorizedException('Person does not exist.');
+    }
+
+    const passwordIsValid = await this.hashingService.compare(
+      LoginDto.password,
+      person.passwordHash,
+    );
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Invalid password.');
+    }
+
+    // Here we will make the new token and we will deliver to the user in the answer
+
+    return {
+      message: 'User logged!!',
+    };
   }
 }
