f31f4cc1dfaddb0df99aa99f7aa4e0a49301ca97
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-12-04 20:08:31 -0300

    feat(auth): implement login flow with Access and Refresh Token generation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,6 +7,7 @@ import { HashingService } from './hashing/hashing.service';
 import jwtConfig from './config/jwt.config';
 import { ConfigType } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Injectable()
 export class AuthService {
@@ -37,21 +38,39 @@ export class AuthService {
       throw new UnauthorizedException('Invalid password.');
     }
 
-    const accessToken = await this.jwtService.signAsync(
+    const accessToken = await this.signJwtAsync<Partial<Person>>(
+      person.id,
+      this.jwtConfiguration.jwtTtl,
+      { email: person.email },
+    );
+
+    const refreshToken = await this.signJwtAsync(
+      person.id,
+      this.jwtConfiguration.jwtRefreshTtl,
+    );
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  private async signJwtAsync<T>(sub: number, expiresIn: number, payLoad?: T) {
+    return await this.jwtService.signAsync(
       {
-        sub: person.id,
-        email: person.email,
+        sub,
+        ...payLoad,
       },
       {
         audience: this.jwtConfiguration.audience,
         issuer: this.jwtConfiguration.issuer,
         secret: this.jwtConfiguration.secret,
-        expiresIn: this.jwtConfiguration.jwtTtl,
+        expiresIn,
       },
     );
+  }
 
-    return {
-      accessToken,
-    };
+  refreshTokens(refreshTokenDto: RefreshTokenDto) {
+    return true;
   }
 }
