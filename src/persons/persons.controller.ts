b3b5a09bdeac12ad3560dadf9abcd063378bc461
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-12-05 22:14:19 -0300

    feat(persons): add support for multipart/form-data upload with FileInterceptor

--- src/persons/persons.controller.ts
+++ src/persons/persons.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   UseGuards,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { PersonsService } from './persons.service';
 import { CreatePersonDto } from './dto/create-person.dto';
@@ -14,6 +16,7 @@ import { UpdatePersonDto } from './dto/update-person.dto';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { TokenPayloadParam } from 'src/auth/params/token-payload.param';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('persons')
 export class PersonsController {
@@ -53,5 +56,21 @@ export class PersonsController {
     @TokenPayloadParam() tokenPayload: TokenPayloadDto,
   ) {
     return this.personsService.remove(+id, tokenPayload);
+  }
+
+  @UseGuards(AuthTokenGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('upload-picture')
+  uploadPicture(
+    @UploadedFile() file: Express.Multer.File,
+    @TokenPayloadParam() tokenPayload: TokenPayloadDto,
+  ) {
+    return {
+      fieldname: file.fieldname,
+      originalname: file.originalname,
+      mimetype: file.mimetype,
+      buffer: {},
+      size: file.size,
+    };
   }
 }
