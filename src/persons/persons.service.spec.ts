826f60f573901426f353eeedb4157b4eebb708ff
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-12-10 05:11:06 -0300

    test(persons): use jest.fn and jest.spyOn to mock method calls and returns in tests

--- src/persons/persons.service.spec.ts
+++ src/persons/persons.service.spec.ts
@@ -4,6 +4,7 @@ import { Person } from './entities/person.entity';
 import { HashingService } from 'src/auth/hashing/hashing.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreatePersonDto } from './dto/create-person.dto';
 
 describe('PersonsService', () => {
   let personService: PersonsService;
@@ -16,11 +17,16 @@ describe('PersonsService', () => {
         PersonsService,
         {
           provide: getRepositoryToken(Person),
-          useValue: {},
+          useValue: {
+            save: jest.fn(),
+            create: jest.fn(),
+          },
         },
         {
           provide: HashingService,
-          useValue: {},
+          useValue: {
+            hash: jest.fn(),
+          },
         },
       ],
     }).compile();
@@ -37,13 +43,36 @@ describe('PersonsService', () => {
   });
 
   describe('create', () => {
-    it('should create a new person', () => {
+    it('should create a new person', async () => {
+      // Arange
       // createPersonDto
+      const createPersonDto: CreatePersonDto = {
+        email: 'pedro@email.com',
+        name: 'Pedro',
+        password: '123456',
+      };
+      const passwordHash = 'PASSHASH';
+
       // Hashing service needs to have the hash method
       // Know if the hashing service was called with createPersonDto
       // Know if the personRepository.create was called with personData
       // Know if personRepository.save was called with the created person
       // The final return must be the new person created -> expect
+
+      jest.spyOn(hashingService, 'hash').mockResolvedValue(passwordHash);
+
+      // Act
+      await personService.create(createPersonDto);
+
+      // Assert
+      expect(hashingService.hash).toHaveBeenCalledWith(
+        createPersonDto.password,
+      );
+      expect(personRepository.create).toHaveBeenCalledWith({
+        name: createPersonDto.name,
+        passwordHash: passwordHash,
+        email: createPersonDto.email,
+      });
     });
   });
 });
